6e64769ad5f5a1d0e019b6adec65cf2519de6820
Soumya <soumya.mondal@1finance.co.in>
2024-06-09 16:37:57 +0530

    db rest b4 another round of int testing

--- src/tests/integration.spec.ts
+++ src/tests/integration.spec.ts
@@ -1,8 +1,14 @@
-import { describe, expect, it } from "vitest";
+import { beforeEach, describe, expect, it } from "vitest";
 import { app } from "../index";
 import request from "supertest";
+import resetDB from "../helpers/resetDB";
 
 describe("POST /sum", () => {
+    beforeEach(async ()=> {
+        console.log('Clearning db')
+        await resetDB();
+        console.log('Cleared DB')
+    })
     it("should sum add 2 numbers", async () => {
         const { status, body } = await request(app).post("/sum").send({ a: 4, b: 9});
         expect(status).toBe(200);
